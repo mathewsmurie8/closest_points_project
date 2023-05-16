6b36055bc0f07591be4d2e5b66e73e948a870b21
mathewsmurie8 <mathewsmurie@gmail.com>
2023-05-16 17:57:09 +0300

    Deploy API

--- closest_points_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ClosestPointsAppConfig(AppConfig):
-    name = 'closest_points_app'
---
+++ closest_points_project/closest_points_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ClosestPointsAppConfig(AppConfig):
+    name = 'closest_points_project.closest_points_app'
