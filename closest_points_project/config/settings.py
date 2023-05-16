d54af08650023ed08deec1c38478a598a5bd5faf
mathewsmurie8 <mathewsmurie@gmail.com>
2023-05-17 00:45:04 +0300

    Simplify database connection settings

--- closest_points_project/config/settings.py
+++ closest_points_project/config/settings.py
@@ -27,33 +27,16 @@ DEBUG = os.getenv("DEBUG", "False") == "True"
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
 
-if DEVELOPMENT_MODE is True:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.postgresql",
-            "NAME": os.path.join(BASE_DIR, "closestpointsdb.postgresql"),
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("NAME", 'crud'),
+        'USER': os.getenv("USER", 'postgres'),
+        'PASSWORD': os.getenv("PASSWORD", 'admin@123'),
+        'HOST': os.getenv("HOST", 'localhost'),
+        'PORT': os.getenv("PORT", '5432'),
     }
-elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
-    if os.getenv("DATABASE_URL", None) is None:
-        raise Exception("DATABASE_URL environment variable not defined")
-    DATABASES = {
-        "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
-    }
-
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'closest_points_db',
-#         'USER': 'postgres',
-#         'PASSWORD': 'admin@123',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
+}
 
 
 # Application definition
